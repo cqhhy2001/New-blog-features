9bf5a52bd6d1152e3b749719b0d8aa95bb09a015
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-04-15 20:55:15 +0200

    Changed animations on the page
    - avatar animation,
    - typewriter effect,

--- app.js
+++ app.js
@@ -11,3 +11,25 @@ function changeTheme() {
 }
 
 themeButton.addEventListener("click", changeTheme);
+
+// Typewriter animation
+
+const text = "QuantumAlgorithm";
+const delay = 200;
+
+const typedText = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+
+let index = 0;
+
+function type() {
+  if (index < text.length) {
+    typedText.textContent += text[index];
+    index++;
+    setTimeout(type, delay);
+  } else {
+    cursor.classList.add("blink");
+  }
+}
+
+type();
